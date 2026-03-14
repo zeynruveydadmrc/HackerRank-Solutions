248fffadf7bcf87f84f3f1fcdf01c4e91d14ecc0
Zeynep Ruveyda Demirci <zeyndemirci@stu.okan.edu.tr>
2026-03-15 00:20:25 +0300

    Create 05-japanese-cities-names.sql

---
+++ SQL/Basic-Select/05-japanese-cities-names.sql
@@ -0,0 +1,6 @@
+-- Problem: Japanese Cities' Names
+-- Link: https://www.hackerrank.com/challenges/japanese-cities-names/problem
+-- Level: Easy
+
+SELECT NAME FROM CITY 
+WHERE COUNTRYCODE = 'JPN';
