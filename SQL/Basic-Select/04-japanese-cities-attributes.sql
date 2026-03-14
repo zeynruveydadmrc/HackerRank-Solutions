cd6f8becad99bbee9f7d5ecaa3469a9e2cb43bf1
Zeynep Ruveyda Demirci <zeyndemirci@stu.okan.edu.tr>
2026-03-15 00:16:27 +0300

    Create 04-japanese-cities-attributes.sql

---
+++ SQL/Basic-Select/04-japanese-cities-attributes.sql
@@ -0,0 +1,5 @@
+-- Problem: Japanese Cities' Attributes
+-- Link: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+-- Level: Easy
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN';
